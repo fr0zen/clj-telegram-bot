d9c77491598e2b699ed2d0c61c525d5100596a73
Alan Meira <alanmeira@gmail.com>
2016-03-10 14:40:52 -0300

    fix json version'

--- project.clj
+++ project.clj
@@ -5,4 +5,4 @@
              :url "https://github.com/fr0zen/clj-telegram-bot/raw/master/LICENSE"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                   [clj-http "2.0.1"]
-                  [org.clojure/data.json "0.2.7"]])
+                  [org.clojure/data.json "0.2.6"]])
